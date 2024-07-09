0ae7d6bc584e93b00f8be6ad915c63c3bac37edd
offic <offic@DESKTOP-GLF4UNU>
2024-07-09 16:13:24 +0600

    Welcome! Introduction to "current date & time".

---
+++ src/main/java/current_date_time/Current_Date_Time.java
@@ -0,0 +1,9 @@
+package current_date_time;
+
+public class Current_Date_Time {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome! Introduction to \"current date & time\".");
+    }
+}
